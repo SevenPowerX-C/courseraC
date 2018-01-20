b4e2721b61270a875fbb4b3752fefe288f1952bc
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2018-01-20 13:47:28 +0100

    Операции с контйнерами, count? sort

--- main.cpp
+++ main.cpp
@@ -1,20 +1,36 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
+
 
 using namespace std;
 
 
 int main() {
     vector<int> nums = {1, 5, 2, 3, 6, 5};
+    int quantity = 0;
     for (int num : nums) {
         cout << num << ", ";
 
     }
-    cout << "\n";
+    for (auto x : nums) {
+        if(x == 5)quantity += 1;
+    }
+    cout << endl;
+    cout << "quantity 5:  = "<<quantity<<endl;
+    cout << "test"<< endl;
+
+    quantity = count(begin(nums), end(nums), 5);
+    cout << endl;
+    cout << "quantity 5:  = "<<quantity<<endl;
+
+    sort(begin(nums), end(nums));
+    for(auto x : nums)cout <<x << ", ";
+    cout <<endl<< "test"<< endl;
+
     vector<string> numStr = {"1", "5", "2", "3", "6", "5+"};
     for (auto num : numStr) {
         cout << num << ", ";
-
     }
     return 0;
 }
